7a08cce9891939631d69dcc7d946289bf30a6d0b
karinatk <karinatkato@gmail.com>
2018-12-12 19:10:04 -0200

    Ajustes gerais no código

--- MessageClearFunctions.py
+++ MessageClearFunctions.py
@@ -1,376 +1,169 @@
 import pandas as pd
 import numpy as np
-import requests, emoji
-import time
-import re, string
-import unicodedata
-import six
+import string
+import re
+import emoji
+from time import time
+from unicodedata import normalize, combining
+from requests import post
 
-def get_json_without_normalization(df):
+def get_json(df, normalization):
     items_list = []
-    for i in range(df.shape[0]):   
+    for idx in range(df.shape[0]):   
         obj = {}
-        obj["configuration"] = {
-            "unicodeNormalization": False,
-            "toLower": True,
-            "informationLevel": 3
+        obj['configuration'] = {
+            'unicodeNormalization': normalization,
+            'toLower': True,
+            'informationLevel': 3
         }
-        obj["id"] = str(df.iloc[i,0])
-        obj["text"] = df.iloc[i,9]
-        obj["dateCheck"] = False
-        items_list.append(obj)
-        
+        obj['text'] = df.loc[idx,'Processed Content']
+        obj['dateCheck'] = False
+        items_list.append(obj)        
     return items_list
-
-def get_json_with_normalization(df):
-    items_list = []
-    for i in range(df.shape[0]):   
-        obj = {}
-        obj["configuration"] = {
-            "unicodeNormalization": True,
-            "toLower": True,
-            "informationLevel": 3
-        }
-        obj["id"] = str(df.iloc[i,0])
-        obj["text"] = df.iloc[i,9]
-        obj["dateCheck"] = False
-        items_list.append(obj)
-        
-    return items_list
-
-
-def lower_case(message):
-    return message.lower()
 
 def remove_emojis(message):
-    emoji_pattern = re.compile("["
-        u"\U0001F600-\U0001F64F"  # emoticons
-        u"\U0001F300-\U0001F5FF"  # symbols & pictographs
-        u"\U0001F680-\U0001F6FF"  # transport & map symbols
-        u"\U0001F1E0-\U0001F1FF"  # flags (iOS)
-                           "]+", flags=re.UNICODE)
-    emoji_pattern.sub(r'', message)    
-    return message
+    emoji_pattern = re.compile('['
+        u'\U0001F600-\U0001F64F'  # emoticons
+        u'\U0001F300-\U0001F5FF'  # symbols & pictographs
+        u'\U0001F680-\U0001F6FF'  # transport & map symbols
+        u'\U0001F1E0-\U0001F1FF'  # flags (iOS)
+                           ']+', flags=re.UNICODE)
+    new_message = emoji_pattern.sub(r'', message)    
+    return new_message
 
 def remove_accentuation(message):
-    new_message = []
-    for word in message.split():
-        new_message.append(remove_accentuation_from_word(word))
-    return ' '.join(new_message)
+    nfkd = normalize('NFKD', message)
+    word_without_accentuation = u''.join([c for c in nfkd if not combining(c)])
+    return word_without_accentuation.strip()
 
-def remove_punctuation(text):
-    text = np.array([text])
-    pattern = "[{}]".format(string.punctuation.replace('@',''))
-    text = [word.lower() for word in text]
-    text = [[re.sub(pattern, ' ', word) for word in words.split()] for words in text]
-    text = [[word for word in words if len(word) > 1] for words in text]
-    text = [' '.join(words) for words in text]
-    text = ' '.join(text)
-    text = text.strip()
-    return text
+def remove_punctuation(message):
+    pattern = '[{}]'.format(string.punctuation.replace('@',''))
+    new_message = re.sub(pattern, ' ', message)
+    return new_message
 
-def remove_accentuation_from_word(word):
-    import unicodedata
-    nfkd = unicodedata.normalize('NFKD', word)
-    word_without_accentuation = u"".join([c for c in nfkd if not unicodedata.combining(c)])
-
-    # Usa expressão regular para retornar a palavra apenas com números, letras e espaço
-    return re.sub('[^a-zA-Z0-9]', '', word_without_accentuation)
-
-
-def convert_lists_in_dic(list_a,list_b):
-    new_dict = {}
-    for element_a,element_b in zip(list_a,list_b):
-        new_dict[element_a] = element_b
-    return new_dict
-
-def remove_abreviations(message):
-    file_name = 'abreviations.txt'
-    abreviations = []
-    correct_word = []
+def use_dictonary_from_file(message, file_name):
+    abreviations_dict = {}
     with open(file_name,'r',encoding='utf-8') as file:
         for relation in file.read().split('\n'):
-            abreviations.append(relation.split(',')[0])
-            correct_word.append(relation.split(',')[1])
-       
-    abreviations_dict = convert_lists_in_dic(abreviations,correct_word)
+            k, v = relation.split(',')
+            abreviations_dict[k] = v
     
     correct_message = []
     for word in message.split():
-        #print(word)
         if word in abreviations_dict.keys():
             correct_message.append(abreviations_dict[word])
         else:
-            correct_message.append(word)
-        
+            correct_message.append(word)        
     return ' '.join(correct_message)
-
+    
+def remove_abreviations(message): 
+    return use_dictonary_from_file(message, 'abbreviations.txt')
 
 def remove_portuguese_errors(message):
-    file_name = 'portuguese_errors.txt'
-    errors = []
-    correct_word = []
-    with open(file_name,'r',encoding='utf-8') as file:
-        for relation in file.read().split('\n'):
-            errors.append(relation.split(',')[0])
-            correct_word.append(relation.split(',')[1])
-       
-    errors_dict = convert_lists_in_dic(errors,correct_word)
-    
-    correct_message = []
-    for word in message.split():
-        if word in errors_dict.keys():
-            correct_message.append(errors_dict[word])
-        else:
-            correct_message.append(word)
-        
-    return ' '.join(correct_message)
+    return use_dictonary_from_file(message, 'portuguese_errors.txt' )
 
-
-def post_processing(message):
-    if type(message) == str:
-       
-        #retirar emojis e emoticons
-        message = remove_emojis(message)
-
-        #remover acentos e pontuação
-        message = remove_accentuation(message)
-        
-        #corrigir abreviações
-        #message = remove_abreviations(message)
-
-        #corrigir erros de digitação
-        #message = remove_portuguese_errors(message)
-
-        # refazer - remover acentos e pontuação
-        #message = remove_accentuation(message)
-        
-        return message
-    else:
-        return message
-
-def pre_processing(message):
-    """ Pre processing function to prepare messages to SmallTalk API request """
-    
-    if type(message) == str:
-
-        message = lower_case(message)
-        message = remove_punctuation(message)
-        message = remove_abreviations(message)
-        message = remove_portuguese_errors(message)
-        
-        return message
-
-    else:
-        return message
-    
-    
-def is_empty(message):
-    return message.isna()  
-
-
-def is_significant(msg):
-    if type(msg) is str:
-        return len(msg) > 2
-    else:
-        return False
-
-    
-def is_trash(msg):
-    if type(msg) is str:
-        if len(msg.split()) > 1:
-            return True
-        else:
-            return False
-    else:
-        return False
-
-    
-def convert_to_int(msg):
-    msg = remove_punctuation(msg)
-    try:
-        integer = int(msg)
-        return True
-    except:
-        return False
-
-
-def is_text_plain(content):
-    if content == 'text/plain':
-        return True
-    else:
-        return False
-
-
-def message_is_empty(message):
-    if type(message) is str:
-        return len(message) == 0
-    else:
-        return True
-    
-
-def is_bot_test(message):
-    word1 = ' teste '
-    word2 = ' take '
-    if type(message) is str:
-        return word1 in message or word2 in message
-    else:
-        return False
-    
-
-def is_uri(message):
-    word1 = ' uri '
-    if type(message) is str:
-        return word1 in message
-    else:
-        return False
-    
-
-def is_numeric(msg):
-    msg = msg.replace('.','').replace('-','').replace('/','').replace(' ','')
-    return msg.isnumeric()
-
-
-def remove_punctuation(text):
-    text = np.array([text])
-    pattern = "[{}]".format(string.punctuation)
-    text = [[re.sub(pattern, ' ', word) for word in words.split()] for words in text]
-    text = [[word for word in words if len(word) > 1] for words in text]
-    text = [' '.join(words) for words in text]
-    text = ' '.join(text)
-    return text
-
-
-def is_number(msg):
-    text = remove_punctuation(msg)
-    return text.isalpha()
-
-
-def smalltalk_requests_without_normalization(data, number_of_batches, request_id):
+def smalltalk_requests(data, api_small_talks, number_of_batches, request_id, normalization):
     
     #Spliting the data into feasible lenght to use in smalltalk api
     data_splitted = np.array_split(data, number_of_batches)
     
     #Calling the API
     r = []
-    i = 0
-    for dataframe in data_splitted:
-        items_list = get_json_without_normalization(dataframe)
+    for idx, dataframe in enumerate(data_splitted):
+        dataframe = dataframe.reset_index()
+        items_list = get_json(dataframe, normalization)
         
-        begin = time.time()
-        obj = {"id": request_id + '_without_norm_' +str(i) , "items": items_list}
-
-        r.append(requests.post('http://hmg-az-infobots.take.net/smalltalks/api/Analysis/batch', json=obj))
-
-        end = time.time()
-        print('Process finished! Time elapsed = ' + str((end - begin)) +' seconds')
-        i = i + 1
-
-    return r
-
-
-def smalltalk_requests_with_normalization(data, number_of_batches, request_id):
-    #Spliting the data into feasible lenght to use in smalltalk api
-    data_splitted = np.array_split(data, number_of_batches)
-    
-    #Calling the API
-    r = []
-    i = 0
-    for dataframe in data_splitted:
-        items_list = get_json_with_normalization(dataframe)
+        begin = time()
+        obj = {'id': str(request_id) + '_' + ('with' if normalization else 'without') + '_norm_' + str(idx) , 'items': items_list}
         
-        begin = time.time()
-        obj = {"id": request_id + '_with_norm_' +str(i) , "items": items_list}
+        r.append(post(api_small_talks, json=obj))
 
-        r.append(requests.post('http://hmg-az-infobots.take.net/smalltalks/api/Analysis/batch', json=obj))
-
-        end = time.time()
+        end = time()
         print('Process finished! Time elapsed = ' + str((end - begin)) +' seconds')
-        i = i + 1
-
     return r
-
-
-def remove_underscore(msg):
-    while '_' in msg:
-        msg = msg.replace('_','')
-    return msg
-
 
 def converting_response_from_API(r):
-    columns = 'MessageId Input CleanedInput RelevantInput MarkedInput'.split()
+    columns = 'Input CleanedInput RelevantInput MarkedInput'.split()
     api_response_data = pd.DataFrame(data = None)
 
-    for message in r.json()['items']:
-        Id = message['id']
+    for message in r['items']:
         Input = message['analysis']['input']
-        CleanedInput = message['analysis']['cleanedInput'].lower()  
-        RelevantInput = message['analysis']['relevantInput'].lower()
-        MarkedInput = message['analysis']['markedInput'].lower()
+        CleanedInput = message['analysis']['cleanedInput']  
+        RelevantInput = message['analysis']['relevantInput']
+        MarkedInput = message['analysis']['markedInput']
         
         matches = message['analysis']['matches']
         if len(matches) > 0:
             for match in matches:
+
+                lenght = match['lenght']
+                
                 if match['smallTalk'] == 'Negation':
                     value = match['value']
                     index = match['index']
-                    lenght = match['lenght']
-                    MarkedInput = MarkedInput[:index] + value + MarkedInput[index + lenght:] #MarkedInput.replace('_' * lenght, value)
-
-                elif match['smallTalk'] == 'Confirmation':
-                    value = match['value']
-                    index = match['index']
-                    lenght = match['lenght']
-                    MarkedInput = MarkedInput[:index] + value + MarkedInput[index + lenght:]    
-                    
+                    MarkedInput = MarkedInput[:index] + value + MarkedInput[index + lenght:]
+                    RelevantInput = RelevantInput[:index] + value + RelevantInput[index + lenght:]
                 else:
-                    value = match['value']
-                    index = match['index']
-                    lenght = match['lenght']
                     MarkedInput = MarkedInput.replace('_' * lenght, '')
-            
-        MarkedInput = remove_underscore(MarkedInput)        
-        line = [Id, Input, CleanedInput, RelevantInput, MarkedInput]
+                    RelevantInput = RelevantInput.replace('_' * lenght, '')
+        line = [Input, CleanedInput, RelevantInput, MarkedInput]
         line_dataframe = pd.DataFrame(data = line)
         api_response_data = api_response_data.append(line_dataframe.T).reset_index(drop = True)
 
     api_response_data.reset_index()
     api_response_data.columns = columns
-    
     return api_response_data
 
+def remove_whatsapp_emojis(message):
+    new_message = [character for character in message if character not in emoji.EMOJI_UNICODE.values()]
+    return ''.join(new_message)
 
-def remove_whatsapp_emojis(msg):
-    if type(msg) is int or type(msg) is float:
-        return msg
-    message = []
-    if len(msg.split()) == 1:
-        msg = remove_emoji_from_word(msg)
-        return msg
-    for word in msg.split():
-        word = remove_emoji_from_word(word)
-        message.append(word)
-    return ' '.join(message).strip()
+def remove_numbers(message):
+    new_message = re.sub(r'[-+]?\d*\.\d+|\d+', ' ', message)
+    return new_message
 
+def remove_spaces(message):
+    new_message = re.sub('\s\s+' , ' ', message)
+    return new_message
 
-def str_strip(msg):
-    if type(msg) is float or type(msg) is int:
-        return msg
-    return msg.strip()
-
-
-def remove_emoji_from_word(teste):
-    word = []
-    for i in range(len(teste)):
-        character = teste[i]    
-        if character in emoji.EMOJI_UNICODE.values():
-            word.append(teste[i].replace(character,' '))
-        else:
-            word.append(character)
-
-    new_word = []        
-    for j in range(len(word)):
-        if word[j] == ' ' or word[j].isalpha() or word[j].isnumeric():
-            new_word.append(word[j])
-    new_word = ''.join(new_word)
-    return new_word
+class Pre_processing:
+    
+    def __init__(self, input_file, api_small_talks = None, content_column = 'Content', encoding = 'utf-8', separator = ';', batch = 4):
+        data = pd.read_csv(input_file, encoding = encoding, sep = separator)
+        self.data = data
+        self.input_file = input_file
+        self.batch = batch
+        self.api_small_talks = api_small_talks
+        if type(content_column) == str: 
+            self.text = data.loc[:, content_column]
+        elif type(content_column) == int:
+            self.text = data.loc[:, content_column]
+        
+    def process(self, output_file, lower = True, punctuation = True, abreviation = True, typo = True, small_talk = True, emojis = True, wa_emojis = True, accentuation = True, number = True, relevant = False):
+        
+        data_processed = pd.DataFrame(data = self.text)
+        data_processed['Processed Content'] = ''
+        for idx, message in enumerate(self.text):
+            if type(message) == str:
+                if emojis: message = remove_emojis(message)
+                if wa_emojis: message = remove_whatsapp_emojis(message)
+                if lower: message = message.lower()
+                if punctuation: message = remove_punctuation(message)
+                if abreviation: message = remove_abreviations(message)
+                if typo: message = remove_portuguese_errors(message)
+                if accentuation: message = remove_accentuation(message)
+                if number: message = remove_numbers(message)
+                message = remove_spaces(message)
+                data_processed.loc[idx, 'Processed Content'] = message 
+        if small_talk and self.api_small_talks is not None:
+            responses = smalltalk_requests(data_processed, self.api_small_talks, self.batch, self.input_file, True)
+            without_small_talks = [converting_response_from_API(response.json()) for response in responses]
+            without_small_talks = pd.concat(without_small_talks).reset_index()
+            if relevant:
+                data_processed['Processed Content'] = without_small_talks.loc[:,'RelevantInput']
+            else:
+                data_processed['Processed Content'] = without_small_talks.loc[:,'MarkedInput']
+            
+        data_processed.to_csv(output_file,sep=';',encoding='utf-8',index=False)
+        
+        
